5646e9f91642d2ffa51aeeb78e0219f82a4ea286
startFromBottom <uhh0701@gmail.com>
2023-07-01 16:47:12 +0900

    remove unused import

--- lib/data/repository/photo_api_repository_impl.dart
+++ lib/data/repository/photo_api_repository_impl.dart
@@ -1,6 +1,4 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:http/http.dart' as http;
-
 import 'package:search_image_app/data/data_source/pixabay_api.dart';
 import 'package:search_image_app/domain/model/photo.dart';
 import 'package:search_image_app/domain/repository/photo_api_repository.dart';
